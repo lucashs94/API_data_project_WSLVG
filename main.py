1d5ce09f6ef514fcdd01718a1f7f62c03eb58b10
lucashs94 <h7.lucas@gmail.com>
2023-11-13 13:22:33 -0300

    endpoint de produtos implementado e testado

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from fastapi import FastAPI
+from typing import List, Dict
 
 app = FastAPI()
+
+
+produtos: List[Dict[str, any]] = [
+    {
+        "id": 1,
+        "nome": "Smartphone",
+        "descricao": "Um otimo telefone",
+        "preco": 1500,
+        "disponivel": True,
+    },
+    {
+        "id": 2,
+        "nome": "Notebook",
+        "descricao": "Um otimo Notebook",
+        "preco": 3500,
+        "disponivel": False,
+    },
+    {
+        "id": 3,
+        "nome": "Tablet",
+        "descricao": "Um otimo Tablet",
+        "preco": 2500,
+        "disponivel": True,
+    },
+]
+
 
 @app.get('/')
 def hello():
     return { 'Ola':'Mundo' }
+
+
+@app.get('/produtos')
+def listar_produtos():
+    return produtos
